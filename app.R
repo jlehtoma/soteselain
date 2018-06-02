ba7f061c692144bba0f78a17c66c4dd924552081
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2018-06-02 16:28:31 +0300

    Initial version of joining stats

--- app.R
+++ app.R
@@ -1,18 +1,42 @@
+library(leaflet)
+library(raster)
+library(pxweb)
+library(RColorBrewer)
+library(R.cache)
+library(sf)
 library(shiny)
 library(shinydashboard)
 library(shinycssloaders)
-library(leaflet)
-library(raster)
-library(RColorBrewer)
-library(sf)
 
 sf_fin <- sf::read_sf("data/kuntajako_2017_maa_alueet_sote.gpkg") %>% 
   dplyr::filter(!is.na(sote_alue))
+
+df_fin <- sf_fin 
+sf::st_geometry(df_fin) <- NULL
 
 # Group by SOTE regions
 sf_fin_sote <- sf_fin %>% 
   dplyr::group_by(sote_alue) %>% 
   dplyr::summarize()
+
+# Get municipal statistics using cached version if available
+query_params <- list(url = "http://pxnet2.stat.fi/PXWeb/api/v1/fi/Kuntien_avainluvut/2017/kuntien_avainluvut_2017_aikasarja.px",
+                     dims = list("Alue 2017" = c('*'),
+                                 Tiedot = c('*'),
+                                 Vuosi = c('*')),
+                     clean = TRUE)
+stat_data <- R.cache::loadCache(query_params)
+if (!is.null(stat_data)) {
+  message("Loaded cached data")
+} else {
+  stat_data <- get_pxweb_data(url = query_params[["url"]],
+                              dims = query_params[["dims"]],
+                              clean = query_params[["clean"]])
+  R.cache::saveCache(stat_data, key = query_params, comment = as.character(Sys.Date()))
+}
+
+stat_variables <- c("Maakunnat", sort(unique(as.character(stat_data$Tiedot))))
+stat_years <- sort(unique(as.character(stat_data$Vuosi)), decreasing = TRUE)
 
 dbHeader <- dashboardHeader(title = "SOTE-selain")
 
@@ -25,11 +49,24 @@ ui <- dashboardPage(
   dashboardBody(
     includeCSS("www/style.css"),
     box(width = 4, title = "SOTE-alueet",
-        checkboxInput("show_munis", "Näytä kunnat", value = TRUE),
+        fluidRow(
+          column(width = 12,
+                 selectInput("select_variable", "Muuttuja", 
+                             choices = stat_variables)
+          ),
+          column(width = 12,
+                 selectInput("select_year", "Vuosi", 
+                             choices = stat_years)
+          ),
+          column(width = 12,
+                 checkboxInput("show_munis", "Näytä kunnat", value = FALSE)
+          )
+        ),
         withSpinner(leafletOutput("sote_alueet", height = 800),
-                    type = 4)
+                    type = 4)  
     ),
-    box(width = 8, title = "Tilastotiedot")
+    box(width = 8, title = "Tilastotiedot"
+    )
   )
 )
 
@@ -44,6 +81,28 @@ server <- function(input, output, session) {
       labeller <- sf_data$sote_alue
     }
     
+    if (input$select_variable == "Maakunnat") {
+      fill_variable <- "sote_alue"
+      pal <- colorFactor(brewer.pal(n = length(unique(sf_data$sote_alue)), name = "Paired"), 
+                         sf_data$sote_alue)
+    } else {
+      dat <- stat_data %>% 
+        dplyr::filter(Tiedot == input$select_variable, 
+                      Vuosi == as.numeric(input$select_year)) %>%
+        dplyr::select(region = `Alue 2017`, variable = Tiedot, values) %>% 
+        dplyr::right_join(df_fin, by = c("region" = "NAMEFIN")) %>% 
+        dplyr::group_by(sote_alue) %>% 
+        dplyr::summarise(
+          values = sum(values)
+        )
+      
+      pal <- colorNumeric("viridis", dat$values)
+      
+      fill_variable <- "values"
+    }
+    
+    sf_data <- dplyr::left_join(sf_data, dat, by = c("sote_alue" = "sote_alue"))
+    
     sf_fin_wgs84 <- sf::st_transform(sf_data, "+init=epsg:4326")
     
     epsg3067 <- leafletCRS(
@@ -52,22 +111,24 @@ server <- function(input, output, session) {
       proj4def = "+proj=utm +zone=35 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs",
       resolutions = 2^(16:7))
     
-    
-    factpal <- colorFactor(brewer.pal(n = length(unique(sf_fin$sote_alue)), name = "Paired"), 
-                           sf_fin$sote_alue)
-    
-    map <- leaflet(sf_fin_wgs84, options = leafletOptions(crs = epsg3067)) %>%
+    map <- leaflet(sf_fin_wgs84, options = leafletOptions(crs = epsg3067,
+                                                          zoomControl = FALSE)) %>%
       addPolygons(weight = 0.5, color = "white", opacity = 1,
                   fillOpacity = 0.9, smoothFactor = 0.5,
-                  fillColor = ~factpal(sote_alue), label = labeller,
-                  labelOptions = labelOptions(direction = "auto")) %>% 
-      addLegend(pal = factpal, values = ~sote_alue, opacity = 1,
-                position = "topleft", title = "")
+                  fillColor = pal(sf_fin_wgs84[[fill_variable]]), label = labeller,
+                  labelOptions = labelOptions(direction = "auto")) 
+      
+      
     return(map)
   }
   
-  # observer_open_review_pdf ---------------------------------------------------
+  # observer_show_munis -------------------------------------------------------
   observeEvent(input$show_munis, {
+    update_map(session, input, output)
+  })
+  
+  # observer_select_variable ---------------------------------------------------
+  observeEvent(input$select_variable, {
     update_map(session, input, output)
   })
   
